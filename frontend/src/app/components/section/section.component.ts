b5a1b0c2fb9aefd97511b839ae101906814f533e
khaled tawashi <khst81@gmail.com>
2018-04-19 23:13:09 +0300

    Complete section mession

--- frontend/src/app/components/section/section.component.ts
+++ frontend/src/app/components/section/section.component.ts
@@ -12,10 +12,10 @@ export class SectionComponent implements OnInit {
   SeoName:string;
   
   sections:Array<Section> = [
-    {id: "sfsdf324234",  name: "تسجيل العلامة التجارية في تركيا",  seoName: "تسجيل العلامة التجارية في تركيا",  description: "تسجيل العلامة التجارية في تركيا",  articleCount: 20,  image: "./assets/images/1113.jpg",  content: "<h1>dsfsf</h1><strong>sdasd asdasd asd</strong>"},
-    {id: "sfsdf32wrwew334",  name: "تأسيس شركة في تركيا",  seoName: "تأسيس شركة في تركيا",  description: "أسيس شركة في تركيا",  articleCount: 30,  image: "./assets/images/4441.jpg",  content: "<h1>dsfsf</h1><strong>sdasd asdasd asd</strong>"},
-    {id: "sfsd3f324234",  name: "طلب الوكالات في تركيا",  seoName: "طلب الوكالات في تركيا",  description: "طلب الوكالات في تركيا",  articleCount: 50,  image: "./assets/images/5552.jpg",  content: "<h1>dsfsf</h1><strong>sdasd asdasd asd</strong>"},
-    {id: "sfsd3f324234",  name: "الإقامات والفيزا في تركيا",  seoName: "الإقامات والفيزا في تركيا",  description: "الإقامات والفيزا في تركيا",  articleCount: 75,  image: "./assets/images/5552.jpg",  content: "<h1>dsfsf</h1><strong>sdasd asdasd asd</strong>"},
+    {id: "sfsdf324234", parentId:"", name: "تسجيل العلامة التجارية في تركيا",  seoName: "تسجيل العلامة التجارية في تركيا",  description: "تسجيل العلامة التجارية في تركيا",  articleCount: 20,  image: "./assets/images/1113.jpg",  content: "<h1>dsfsf</h1><strong>sdasd asdasd asd</strong>"},
+    {id: "sfsdf32wrwew334", parentId:"", name: "تأسيس شركة في تركيا",  seoName: "تأسيس شركة في تركيا",  description: "أسيس شركة في تركيا",  articleCount: 30,  image: "./assets/images/4441.jpg",  content: "<h1>dsfsf</h1><strong>sdasd asdasd asd</strong>"},
+    {id: "sfsd3f324234", parentId:"", name: "طلب الوكالات في تركيا",  seoName: "طلب الوكالات في تركيا",  description: "طلب الوكالات في تركيا",  articleCount: 50,  image: "./assets/images/5552.jpg",  content: "<h1>dsfsf</h1><strong>sdasd asdasd asd</strong>"},
+    {id: "sfsd3f324234", parentId:"", name: "الإقامات والفيزا في تركيا",  seoName: "الإقامات والفيزا في تركيا",  description: "الإقامات والفيزا في تركيا",  articleCount: 75,  image: "./assets/images/5552.jpg",  content: "<h1>dsfsf</h1><strong>sdasd asdasd asd</strong>"},
   ];
   constructor(
     private route: ActivatedRoute
